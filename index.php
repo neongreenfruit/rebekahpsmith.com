0655a0f04c6a39eb90b6d5097b504e771eced8de
Bekah <neongreenfruit@gmail.com>
2015-04-28 13:08:14 -0400

    First Commit to Website

---
+++ index.php
@@ -0,0 +1,339 @@
+<?php
+include "../private_html/bootstrap.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" type="image/png" href="public_html/picturesMain/australia1.png" />
+    <title>Rebekah Smith | Home</title>
+    
+    <!-- Bootstrap core CSS -->
+    <link href= "public_html/css/bootstrap.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="public_html/css/main.css" rel="stylesheet">
+    
+	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+    
+    <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Raleway:400,300,700' rel='stylesheet' type='text/css'>
+    
+    <script src="public_html/js/jquery.min.js"></script>
+    <script type="text/javascript" src="public_html/js/smoothscroll.js"></script>
+    <script src="public_html/js/Chart.js"></script>
+  </head>
+
+  <body data-spy="scroll" data-offset="0" data-target="#nav">
+
+	<div id="section-topbar">
+		<div id="topbar-inner">
+			<div class="container">
+				<div class="row">
+					<div class="dropdown">
+						<ul id="nav" class="nav">
+							<li class="menu-item"><a class="smoothScroll" href="#about" title="About"><i class="fa fa-user"></i></a></li>
+							<li class="menu-item"><a class="smoothScroll" href="#resume" title="Resume"><i class="fa fa-file"></i></a></li>
+							<li class="menu-item"><a class="smoothScroll" href="#work" title="Work"><i class="fa fa-suitcase"></i></a></li>
+							<li class="menu-item"><a class="smoothScroll" href="#photo" title="Photos"><i class="fa fa-camera"></i></a></li>
+							<li class="menu-item"><a class="smoothScroll" href="#contact" title="Contact"><i class="fa fa-envelope"></i></a></li>
+						</ul><!--/ uL#nav -->
+					</div><!-- /.dropdown -->
+
+					<div class="clear"></div>
+				</div><!--/.row -->
+			</div><!--/.container -->
+
+			<div class="clear"></div>
+		</div><!--/ #topbar-inner -->
+	</div><!--/ #section-topbar -->
+	
+	<div id="headerwrap">
+		<div class="container">
+			<div class="row centered">
+				<div class="col-lg-12">
+					<h1>Rebekah Paige Smith</h1>
+					<h3>Computer Information Systems Major | <a id="specialmailto" href="mailto:resume@rebekahpsmith.com">resume@rebekahpsmith.com</a></h3>
+				</div><!--/.col-lg-12 -->
+			</div><!--/.row -->
+		</div><!--/.container -->
+	</div><!--/.#headerwrap -->
+
+    <div id="about"></div>
+	<div id="intro">
+		<div class="container">
+			<div class="row centered">
+				<div class="col-lg-12">
+					<h3>ABOUT</h3>
+				</div>
+				<div class="col-lg-8 col-lg-offset-2">
+					<hr>
+					<p> My name is Rebekah Paige Smith, I am a Senior Computer Information Systems Major with
+                        concentrations in Computer Science and Software Development at Messiah College.<br/><br/>In the next
+                        few years I hope to have the opportunity to be a part of a Software Development team. I'm not
+                        sure exactly what I would like working on, but I love having the opportunity to get passionate
+                        about a project. It would be my dream to not only get to design software, but be passionate
+                        about the work that I am doing.<br/><br/>I have had a passion for traveling since I was very young.
+                        I got the opportunity to study abroad in Australia in the Fall of 2013. I feel that this
+                        experience has changed my life and opened my eyes to life outside of the United States. I hope
+                        to continue traveling over the course of my entire life. Every trip that I have taken has been
+                        life changing, and I do not doubt that they will continue to be.
+                    </p>
+				</div>
+			</div><!--/.row -->
+		</div><!--/.container -->
+	</div><!--/ #intro -->
+
+	<div id="resume"></div>
+	<!--EDUCATION DESCRIPTION -->
+	<div class="container desc">
+		<div class="row">
+
+				<div class="col-lg-11 col-lg-offset-1">
+					<h4>EDUCATION</h4>
+					</br>
+				</div>
+				<div class="col-lg-6 col-lg-offset-2">
+					<p class="titles">Computer and Information Systems: Computer Science<br/>
+						<a href="http://www.messiah.edu/computer-and-information-science-major-pennsylvania" title="Messiah College">Messiah College</a> <br/>
+						<i>Mechanicsburg, Pennsylvania</i>
+					</p>
+				</div>
+				<div class="col-lg-4">
+					<p class="dates">GRADUATING IN MAY 2015<br/>
+					<i><b>IN PROGRESS</b></i>
+					</p>
+				</div>
+	
+				<div class="col-lg-6 col-lg-offset-2">
+					<p class="titles">Study Abroad<br/>
+						<a href="http://www.wi.edu.au/" title="Wesley Institute">Wesley Institute</a> <br/>
+						<i>Sydney, New South Wales, Australia</i>
+					</p>
+				</div>
+				<div class="col-lg-4">
+					<p class="dates">JULY 2013 - NOVEMBER 2013</p>
+				</div>
+	
+		</div><!--/.row -->
+		<br>
+		<hr>
+	</div><!--/.container -->
+	
+	<div id="work"></div>
+	<!--WORK DESCRIPTION -->
+	<div class="container desc">
+		<div class="row">
+
+				<div class="col-lg-11 col-lg-offset-1">
+					<h4>WORK</h4>
+					</br>
+				</div>
+				<div class="col-lg-6 col-lg-offset-2">
+                    <h4 class="titles">IT Fellow<br/></h4>
+                    <h5><a href="https://cure.org/" title="Cure International">Cure International</a><br/></h5>
+                    <hr class="hrworksection">
+                    <p>
+                            Over the summer I will be working with Cure, by traveling to any of 
+                            their hospitals and doing IT work. I will also be doing software development
+                            on various projects that are currently happening.
+                    </p>
+                </div>
+                <div class="col-lg-4">
+                    <p class="dates">STARTING MAY 2015</p>
+                </div>
+				
+				<div class="col-lg-6 col-lg-offset-2">
+                    <h4 class="titles">Software Development Intern<br/></h4>
+                    <h5><a href="https://cure.org/" title="Cure International">Cure International</a><br/></h5>
+                    <hr class="hrworksection">
+                    <p>
+                            Currently I am working on a Hospital Records Software called 
+                            <a href="http://hospitalrun.io/" title="Hospital Run">HospitalRun</a>.
+                            As part of my senior capstone class I have been working on a piece of 
+                            this software to help the Cure Hospitals keep track of ministry data. 
+                            Currently they keep track of this data in spreadsheets, and we are 
+                            helping them use an interface that it much more user friendly.
+                    </p>
+                </div>
+                <div class="col-lg-4">
+                    <p class="dates">JANUARY 2015 – PRESENT</p>
+                </div>
+        		<hr>
+                
+                <div class="col-lg-6 col-lg-offset-2">
+                    <h4 class="titles">Intern - Intelligent Water Project<br/></h4>
+                    <h5><a href="http://www.messiah.edu/info/21317/collaboratory_new" title="The Collaboratory">
+                            The Collaboratory for Strategic Partnerships and Applied Research</a> <br/></h5>
+                    <hr class="hrworksection">
+                    <p>
+                            The Intelligent Water Project is a system that takes advantage of the existing GSM network
+                            in Africa to monitor the functionality of village hand pumps and sends SMS Alerts to
+                            stakeholders in the event of a failure. I will be working on the SMS transport layer. This
+                            will require creating a combined hardware/software solution that can be installed in Ghana
+                            that moves SMS messages from the remote pump monitors to the web back end and from the web
+                            back end to the stakeholders.
+                    </p>
+                </div>
+                <div class="col-lg-4">
+                    <p class="dates">SEPTEMBER 2014 – DECEMBER 2014</p>
+                </div>
+
+				<div class="col-lg-6 col-lg-offset-2">
+					<h4 class="titles">Help Desk - Mac Technician<br/></h4>
+					<h5><a href="http://www.messiah.edu/info/20154/information_technology_services" title="Messiah College">Messiah College</a><br/></h5>
+					<hr class="hrworksection">
+					<p>
+							Respond to issues people have with their Mac computers. 
+							Usually that involves going over and taking a look at the 
+							issue they have in order to fix it. I was hired to work with 
+							Macs, but have been gaining knowledge with the PC side of 
+							things.
+					</p>
+				</div>
+				<div class="col-lg-4">
+					<p class="dates">OCTOBER 2012 – PRESENT</p>
+				</div>
+	
+				<div class="col-lg-6 col-lg-offset-2">
+					<h4 class="titles">Teacher's Assistant<br/></h4>
+					<h5><a href="http://www.messiah.edu" title="Messiah College">Messiah College</a><br/></h5>
+					<hr class="hrworksection">
+					<p>
+							I grade assignments for Dr. Weaver's Web Development 1: 
+							Client Side.
+					</p>
+				</div>
+				<div class="col-lg-4">
+					<p class="dates">FEBRUARY 2014 – PRESENT</p>
+				</div>
+				
+				<div class="col-lg-6 col-lg-offset-2">
+					<h4 class="titles">Sales Professional<br/><h4>
+					<h5><a href="http://www.brinestone.net/index.html/" title="Brinestone">Brinestone</a><br/></h5>
+					<hr class="hrworksection">
+					<p>
+							I talked to people, figured out why they were looking into 
+							buying a computer, and helped them make the best decision on 
+							which computer fit their needs.
+					</p>
+				</div>
+				<div class="col-lg-4">
+					<p class="dates">MARCH 2011 - AUGUST 2012</p>
+				</div>
+				
+				<div class="col-lg-6 col-lg-offset-2">
+					<h4 class="titles">Software Development - Intern<br/><h4>
+					<h5><a href="https://www.gatherworks.com/" title="Gatherworks">Gatherworks</a><br/></h5>
+					<hr class="hrworksection">
+					<p>
+							I did some coding, but mostly I was learning good ways to 
+							research, and I learned a lot about the industry of software 
+							development.
+					</p>
+				</div>
+				<div class="col-lg-4">
+					<p class="dates">SEPTEMBER 2010 - JANUARY 2011</p>
+				</div>
+		</div><!--/.row -->
+		<br>
+		<hr>
+	</div><!--/.container -->
+
+	<div id="photo"></div>
+	<!--PHOTO GALLERY DESCRIPTION -->
+	<div class="container desc">
+		<div class="row">
+				<div class="col-lg-11 col-lg-offset-1">
+					<h4>PHOTO GALLERY</h4>
+					</br>
+				</div>
+				<div class="col-lg-6 col-lg-offset-2">
+					<p><img class="img-responsive" src="public_html/img/RPS_NZ.jpg" alt=""></p>
+				</div>
+				<div class="col-lg-4">
+					<p class="titles">STUDYING ABROAD - NEW ZEALAND</p>
+				<p>
+                    Rotorua, New Zealand<br/><br/>
+					<i class="fa fa-tag"></i> NZ
+				</p>
+				</div>
+	
+				<div class="col-lg-6 col-lg-offset-2">
+					<p><img class="img-responsive" src="public_html/img/ASC_FAVE.jpg" alt=""></p>
+				</div>
+				<div class="col-lg-4">
+                    <p class="titles">STUDYING ABROAD - SYDNEY</p>
+                    <p>
+                        This is my favourite picture from Australia. I love how happy everyone looks and I love
+                        that it is totally candid.<br/><br/>
+                        <i class="fa fa-tag"></i> AUS
+                    </p>
+				</div>
+				
+				<div class="col-lg-6 col-lg-offset-2">
+					<p><img class="img-responsive" src="public_html/img/NEWCASTLE.jpg" alt=""></p>
+				</div>
+				<div class="col-lg-4">
+						<p class="titles">NEWCASTLE</p>
+					<p>
+                        Beautiful isn't it? <br/><br/>
+						<i class="fa fa-tag"></i> AUS
+					</p>
+				</div>
+		
+		</div><!--/.row -->
+		<br>
+		<br>
+	</div><!--/.container -->
+
+	<div id="contact"></div>
+	<!--FOOTER DESCRIPTION -->
+	<div id="footwrap">
+		<div class="container">
+			<div class="row">
+			
+				<div class="col-lg-11 col-lg-offset-1">
+					<h4>CONTACT INFO AND OTHER LINKS</h4>
+					</br>
+				</div>
+				<div class="col-lg-6 col-lg-offset-2">
+					<p><b>Email</b><br/>
+						<a href="mailto:resume@rebekahpsmith.com">resume@rebekahpsmith.com</a><br/>
+					</p>
+                    <p><b>Internship Portfolio Website</b><br/>
+                        <a href="https://sites.google.com/a/messiah.edu/rps-eli/">https://sites.google.com/a/messiah.edu/rps-eli/</a> <br/>
+                    </p>
+                    <p><b>Web Development 2 Blog</b><br/>
+                        <a href="http://blog.rebekahpsmith.com/">http://blog.rebekahpsmith.com/</a> <br/>
+                    </p>
+				</div>
+				<div class="col-lg-4">
+					<h5>SOCIAL LINKS</h5>
+					<p>
+						<a href="https://twitter.com/rebekahpsmith"><i class="fa fa-twitter"></i></a>
+						<a href="http://instagram.com/musicisformyjesus"><i class="fa fa-instagram"></i></a>
+						<a href="https://www.linkedin.com/in/neongreenfruit"><i class="fa fa-linkedin-square"></i></a>
+						<a href="https://github.com/neongreenfruit"><i class="fa fa-github"></i></a>
+					</p>
+				</div>
+			</div><!--/.row -->
+		</div><!--/.container -->
+	</div><!--/ #footer -->
+	
+	<div id="c">
+		<div class="container">
+			<p>Created by <a href="http://www.blacktie.co">BLACKTIE.CO</a></p>
+		
+		</div>
+	</div>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="public_html/js/bootstrap.js"></script>
+  </body>
+</html>
---
+++ private_html/bootstrap.php
@@ -0,0 +1,7 @@
+<?php
+#
+# Define Path Constants
+
+define('BASE_PATH', '/Volumes/home/rs1381/web/main');
+define('WEB_PATH', BASE_PATH . 'public_html/');
+define('WEB_ROOT', 'http://home.messiah.edu/~rs1381/main/');
